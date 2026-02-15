33ea10ef84a27b5399618ca330d597af135dac33
Omkar Pawar <pawarnandkumaromved@gmail.com>
2026-02-15 20:03:41 +0530

    Simplify CORS configuration in SecurityConfig

--- src/main/java/com/project/society/config/SecurityConfig.java
+++ src/main/java/com/project/society/config/SecurityConfig.java
@@ -44,8 +44,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable())
-                .cors(cors -> {
-                })
+                .cors(Customizer.withDefaults()) 
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
 
